d86abf95a172bc483f933ddd2d5e20b97b72270c
Matheus Carneiro <76215464+mcarneir0@users.noreply.github.com>
2022-05-10 23:22:06 -0300

    Adicionado Métodos CRUD para os filmes

--- pages/api/movies.js
+++ pages/api/movies.js
@@ -1,15 +1,61 @@
-import connectToDatabase from "../../lib/mongodb";
+import clientPromise from "../../lib/mongodb";
 
-export default async function (req, res) {
+//  Variável de Ambiente que armazena o nome do banco de dados 
+const mongodb = process.env.MONGODB_DB
+
+export default async function handler(req, res) {
     
-    const db = await connectToDatabase();
+    //  Informações do Banco
+    const client = await clientPromise;
+    const db = client.db(mongodb);
 
-    if (req.method == "GET") {
-        const movies = await db
-            .collection("movies")
-            .find({ year: 2015})
-            .sort({ title: 1 })
-            .toArray();
-        res.json(movies);
+    switch (req.method) {
+
+        case "GET":
+            //  Realiza o SELECT (READ)
+            
+            const movies = await db
+                .collection("movies")
+                .find({ year: 2015 })
+                .sort({ title: 1 })
+                .limit(50)
+                .toArray();
+            res.json({ body: movies});
+            break;
+        
+        case "POST":
+            //  Realiza o INSERT INTO (CREATE)
+            
+            const novoFilme = JSON.parse(req.body);
+            const inserirFilme = await db
+                .collection("movies")
+                .insertOne(novoFilme);
+            res.json({ body: inserirFilme });
+            break;
+
+        case "PUT":
+            //  Realiza o UPDATE
+
+            const filmeAtualizado = JSON.parse(req.body);
+            const atualizarFilme = await db
+                .collection("movies")
+                .updateOne({ 
+                    title: filmeAtualizado.title }, {
+                        $set:
+                            { lastupdated: filmeAtualizado.lastupdated }
+                    }
+                );
+            res.json({ body: atualizarFilme });
+            break;
+
+        case "DELETE":
+            //  Realiza o DELETE
+
+            const filmeDeletado = JSON.parse(req.body);
+            const deletarFilme = await db
+                .collection("movies")
+                .deleteOne({ title: filmeDeletado.title });
+            res.json({ body: deletarFilme });
+            break;
     }
 }
