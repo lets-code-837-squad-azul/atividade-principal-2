2ce023980d360e2fa49013461fc5d71bd257bcb3
DECIO SANTANA DE AGUIAR <88034016+Decioagu@users.noreply.github.com>
2022-05-12 23:21:22 -0300

    Final Fianl

--- pages/api/movies/index.js
+++ pages/api/movies/index.js
@@ -24,6 +24,7 @@ export default async function handler(req, res) {
                 .sort({ title: 1 })
                 .limit(filtro_limit)
                 .toArray();
+                console.log(movies)
                 res.json({ body: movies});      
             break;
         
--- pages/filmes.js
+++ pages/filmes.js
@@ -52,19 +52,13 @@ export default function Home({ movies }) {
     console.log("DELETE =>", res);
   }
 
-  /* const get = async () => {
+  const get = async () => {
     //  recebendo dados utilizando metodo GET
     const ano = input_get_ano.current.value;
     const limit = input_get_limit.current.value;
-    const res = await fetch(URL + "api/movies", {
-      method: "GET",
-      body: JSON.stringify({
-        limit: limit,
-        year: ano
-      })
-    })
+    const res = await fetch(URL + "api/movies/ano="+ano+"&limit="+limit)
     console.log("GET =>", res);
-  } */
+  }
 
   return (
     <div className="container">
@@ -94,13 +88,13 @@ export default function Home({ movies }) {
 
       <br></br>
 
-      {/* <div>
+      <div>
         <label>Definir ano:</label>
         <input type="number" ref={input_get_ano} ></input>
         <label>&ensp; Tamanho da lista de filmes:</label>
         <input type="number" ref={input_get_limit} ></input>
         <button type="submit" onClick={get}>PESQUISAR</button>
-      </div> */}
+      </div>
 
       <ul>
         {lista_de_filmes.map((movie, index) => {
